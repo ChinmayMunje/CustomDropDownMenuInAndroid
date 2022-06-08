812a8849aba82d06430ceadff08b437f6e870272
Chinmay Munje <chinmaymunje96@gmail.com>
2022-06-08 19:41:26 +0530

    Simple and Material Date Picker Example.

--- app/src/main/java/com/gtappdevelopers/googlemapsroutes/DatePickerActivity.java
+++ app/src/main/java/com/gtappdevelopers/googlemapsroutes/DatePickerActivity.java
@@ -9,31 +9,63 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class DatePickerActivity extends AppCompatActivity {
 
-    private LinearLayout dateLL;
-    private TextView selectedDateTV;
+    private LinearLayout simpleDateLL, materialDateLL;
+    private TextView simpleDateTV, materialDateTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_picker);
-        dateLL = findViewById(R.id.idLLDate);
-        selectedDateTV = findViewById(R.id.idTVDate);
-        dateLL.setOnClickListener(new View.OnClickListener() {
+        simpleDateLL = findViewById(R.id.idLLDate);
+        materialDateLL = findViewById(R.id.idLLMaterialDatePicker);
+        simpleDateTV = findViewById(R.id.idTVDate);
+        materialDateTV = findViewById(R.id.idTVMaterialDatePicker);
+        simpleDateLL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 datePicker();
             }
         });
+        materialDateLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker();
+            }
+        });
+    }
 
+    private void materialDatePicker() {
+        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
+        materialDateBuilder.setTitleText("Select a date");
+        MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+        materialDatePicker.show(getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                String date = materialDatePicker.getHeaderText();
+                SimpleDateFormat input = new SimpleDateFormat("dd MMM yyyy");
+                SimpleDateFormat output = new SimpleDateFormat("dd/MM/yyyy");
+                try {
+                    materialDateTV.setText(output.format(input.parse(date)));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void datePicker() {
         final Calendar calendar = Calendar.getInstance();
-        int mYear, mMonth, mDay, mHour, mMinute;
+        int mYear, mMonth, mDay;
         mYear = calendar.get(Calendar.YEAR);
         mMonth = calendar.get(Calendar.MONTH);
         mDay = calendar.get(Calendar.DAY_OF_MONTH);
@@ -42,7 +74,7 @@ public class DatePickerActivity extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
-                selectedDateTV.setText(selectedDate);
+                simpleDateTV.setText(selectedDate);
             }
         }, mYear, mMonth, mDay);
         datePickerDialog.show();
